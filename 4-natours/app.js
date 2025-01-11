db409fea988c5ded07cc5e6e03980d2010381f41
Clint <clinthiles1992@gmail.com>
2025-01-11 09:54:14 -0800

    Initial commit

---
+++ 4-natours/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to the endpoing...');
+}); // hello
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App running on PORT ${PORT}.......`);
+});
+// 127.0.0.1:3000
