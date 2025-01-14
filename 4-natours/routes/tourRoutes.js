3e8d1a1227322bbf431ccd516675e5565d2cd064
Clint <clinthiles1992@gmail.com>
2025-01-14 10:14:00 -0800

    feat: add checkBody middleware to validate tour creation input

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -5,12 +5,17 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body conatains the name and price property
+// If not, send back a 400 bad request
+// Add it to the post handler stack
+
 // Define routes
 // prettier-ignore
 router
 .route('/')
 .get(tourController.getAllTours)
-.post(tourController.createTour);
+.post(tourController.checkBody, tourController.createTour);
 
 // prettier-ignore
 router
